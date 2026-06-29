bf59f0e7269175866482a60507e7566c1d5920a0
LaxNaraWini <laxnarstudio@gmail.com>
2026-06-29 19:04:33 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCVTFEseCU0PlXF7b5GAuGxmD4_QO_1XLQ",
+    authDomain: "laxnara-wini-dairy.firebaseapp.com",
+    projectId: "laxnara-wini-dairy",
+    storageBucket: "laxnara-wini-dairy.firebasestorage.app",
+    messagingSenderId: "774501530597",
+    appId: "1:774501530597:web:cb044ef436aa0d0f3fcd0c"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
